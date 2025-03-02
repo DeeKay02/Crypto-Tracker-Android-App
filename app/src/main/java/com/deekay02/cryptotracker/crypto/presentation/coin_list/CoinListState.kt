b3a95590b13607ff909ad69c5fbd57f7c50691fd
DeeKay02 <48171972+deekay02@users.noreply.github.com>
2025-03-03 02:10:42 +0530

    Implement coin list screen UI

---
+++ app/src/main/java/com/deekay02/cryptotracker/crypto/presentation/coin_list/CoinListState.kt
@@ -0,0 +1,11 @@
+package com.deekay02.cryptotracker.crypto.presentation.coin_list
+
+import androidx.compose.runtime.Immutable
+import com.deekay02.cryptotracker.crypto.presentation.models.CoinUi
+
+@Immutable
+data class CoinListState(
+    val isLoading: Boolean = false,
+    val coins: List<CoinUi> = emptyList(),
+    val selectedCoin: CoinUi? = null
+)
